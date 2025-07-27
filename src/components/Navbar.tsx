1971fba2ec4aa1b155df429dbdc6275593882efe
Railson Albino <railsonvlogz@gmail.com>
2025-07-27 01:43:30 -0300

    🔧 refactor: update Navbar component to use React Router for navigation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,15 @@
-
+import { Link } from 'react-router-dom';
   const Navbar = () => {
     return (
       <nav className="bg-black p-6 text-white border-b-2">
         <div>
-            <h1 className="text-xl font-extralight hover:text-blue-500 ease-in-out transition-all duration-300 cursor-pointer font-mono">R</h1>
+            <h1 className="text-xl font-extralight hover:text-blue-500 ease-in-out transition-all duration-300 cursor-pointer font-mono">mec.dev</h1>
         </div>
         <div className="flex justify-end items-center">
             <ul className="flex space-x-4 text-sm font-mono -mt-7">
-                <li><a href="#home" className="hover:text-blue-500 ease-in-out transition-all duration-300">home</a></li>
+                <li><Link className="hover:text-blue-500 ease-in-out transition-all duration-300" to="/">home</Link></li>
                 <li><a href="#about" className="hover:text-blue-500 ease-in-out transition-all duration-300">about</a></li>
-                <li><a href="#projects" className="hover:text-blue-500 ease-in-out transition-all duration-300">projects</a></li>
+                <li><Link className="hover:text-blue-500 ease-in-out transition-all duration-300" to="/projetos">projects</Link></li>
                 <li><a href="#contact" className="hover:text-blue-500 ease-in-out transition-all duration-300">contact</a></li>
             </ul>
         </div>
