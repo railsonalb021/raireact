5fe3d38f43e38a0508dc15ca9d87dbcba748421c
Railson Albino <railsonvlogz@gmail.com>
2025-07-27 01:43:44 -0300

    🎨 update HomePage layout and content for improved presentation

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -11,15 +11,15 @@ const HomePage = () => {
         <img src={minhaImagem} alt="Railson Albino" className="rounded-full w-48 h-32 mb-4" />
       </div>
 
-      <div className='grid grid-cols-3 lg:pt-20'>
+      <div className='grid grid-cols-1 lg:grid-cols-3 lg:pt-20'>
       <div className='flex flex-col items-start pl-10'>
         <h1 className='text-blue-500 pl-24 pb-3 font-mono'>my skills</h1>
         <p className='text-white font-mono'>my skills are developing projects <br></br> in vue, nuxt, typescript and tailwind</p>
       </div>
 
       <div className='flex flex-col items-center pl-10'>
-        <h1 className='text-blue-500 pr-5 pb-3 font-mono'>my skills</h1>
-        <p className='text-white font-mono'>my skills are developing projects <br></br> in vue, nuxt, typescript and tailwind</p>
+        <h1 className='text-blue-500 pr-5 pb-3 font-mono'>about me</h1>
+        <p className='text-white font-mono'>I study computer science, but I work as a web <br></br> development intern, I'm 22 years old</p>
       </div>
 
       <div className='flex flex-col items-center pl-10'>
