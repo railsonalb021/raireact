162b6c182ddb956fdcea6c8f344f41a3d0bb7a83
Railson Albino <railsonvlogz@gmail.com>
2025-07-28 12:28:21 -0500

    🔧 refactor: update Navbar component to use function declaration and localization for text

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,23 @@
-import { Link } from 'react-router-dom';
-  const Navbar = () => {
-    return (
-      <nav className="bg-black p-6 text-white border-b-2">
-        <div>
-            <h1 className="text-xl font-extralight hover:text-blue-500 ease-in-out transition-all duration-300 cursor-pointer font-mono">mec.dev</h1>
-        </div>
-        <div className="flex justify-end items-center">
-            <ul className="flex space-x-4 text-sm font-mono -mt-7">
-                <li><Link className="hover:text-blue-500 ease-in-out transition-all duration-300" to="/">home</Link></li>
-                <li><a href="#about" className="hover:text-blue-500 ease-in-out transition-all duration-300">about</a></li>
-                <li><Link className="hover:text-blue-500 ease-in-out transition-all duration-300" to="/projetos">projects</Link></li>
-                <li><a href="#contact" className="hover:text-blue-500 ease-in-out transition-all duration-300">contact</a></li>
-            </ul>
-        </div>
-      </nav>
-    );
-  };
-  
-  export default Navbar;
+import { useTranslation } from 'react-i18next'
+import { Link } from 'react-router-dom'
+
+function Navbar(){
+  const { t } = useTranslation()
+  return (
+    <nav className="border-b-2 bg-black p-6 text-white">
+      <div>
+        <h1 className="cursor-pointer font-mono text-xl font-extralight transition-all duration-300 ease-in-out hover:text-blue-500">mec.dev</h1>
+      </div>
+      <div className="flex items-center justify-end">
+        <ul className="-mt-7 flex space-x-4 font-mono text-sm">
+          <li><Link className="transition-all duration-300 ease-in-out hover:text-blue-500" to="/">{t('home')}</Link></li>
+          <li><a href="#about" className="transition-all duration-300 ease-in-out hover:text-blue-500">{t('about')}</a></li>
+          <li><Link className="transition-all duration-300 ease-in-out hover:text-blue-500" to="/projetos">{t('projects')}</Link></li>
+          <li><a href="#contact" className="transition-all duration-300 ease-in-out hover:text-blue-500">{t('contact')}</a></li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
