ecee58d13dde9a8f3caa94d2eb82f397959c84b8
Railson Albino <railsonvlogz@gmail.com>
2025-07-28 21:16:50 -0300

    🔧 feat: update language selection buttons in App component to use flag images for better UX

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import Footer from './components/Footer'
 import Navbar from './components/Navbar'
 import HomePage from './pages/HomePage'
 import ProjectPage from './pages/ProjectPage'
+import imagembr from './assets/br.png'
+import imagemusa from './assets/us.png'
 
 function App(){
   const { i18n } = useTranslation()
@@ -16,8 +18,12 @@ function App(){
     <Router>
       <Navbar />
       <div className="flex justify-end gap-2 px-4 py-2">
-        <button type='button' onClick={() => changeLanguage('pt')} className="text-2xl transition-all duration-300 ease-out hover:scale-125">🇧🇷</button>
-        <button type='button' onClick={() => changeLanguage('en')} className="text-2xl transition-all duration-300 ease-out hover:scale-125">🇺🇸</button>
+        <button type='button' onClick={() => changeLanguage('pt')} className="text-2xl transition-all duration-300 ease-out hover:scale-125">
+          <img src={imagembr} alt="Portuguese" className="h-6 w-8 rounded-full" />
+        </button>
+        <button type='button' onClick={() => changeLanguage('en')} className="text-2xl transition-all duration-300 ease-out hover:scale-125">
+        <img src={imagemusa} alt="estadosunidos" className="h-6 w-8 rounded-full" />
+        </button>
       </div>
       <Routes>
         <Route path="/" element={<HomePage />} />
