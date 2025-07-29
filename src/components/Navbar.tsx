6b06a78596ed63e57267fc0b49e201403dec44ad
Railson Albino <railsonvlogz@gmail.com>
2025-07-28 21:17:12 -0300

    🔧 refactor: replace static text with TranslatedText component for localization in Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,20 @@
-import { useTranslation } from 'react-i18next'
+import TranslatedText from '../components/TranslatedText'
 import { Link } from 'react-router-dom'
 
 function Navbar(){
-  const { t } = useTranslation()
   return (
     <nav className="border-b-2 bg-black p-6 text-white">
       <div>
-        <h1 className="cursor-pointer font-mono text-xl font-extralight transition-all duration-300 ease-in-out hover:text-blue-500">mec.dev</h1>
+        <TranslatedText textKey='mec.dev' className="cursor-pointer font-mono text-xl font-extralight transition-all duration-300 ease-in-out hover:text-blue-500"></TranslatedText>
       </div>
       <div className="flex items-center justify-end">
         <ul className="-mt-7 flex space-x-4 font-mono text-sm">
-          <li><Link className="transition-all duration-300 ease-in-out hover:text-blue-500" to="/">{t('home')}</Link></li>
-          <li><a href="#about" className="transition-all duration-300 ease-in-out hover:text-blue-500">{t('about')}</a></li>
-          <li><Link className="transition-all duration-300 ease-in-out hover:text-blue-500" to="/projetos">{t('projects')}</Link></li>
-          <li><a href="#contact" className="transition-all duration-300 ease-in-out hover:text-blue-500">{t('contact')}</a></li>
+        <Link to="/">
+        <TranslatedText textKey='home' className='hover:text-blue-500 ease-out transition-all duration-300 cursor-pointer' /></Link>
+        <TranslatedText textKey='about' className='hover:text-blue-500 ease-out transition-all duration-300 cursor-pointer' />
+        <Link to="/projetos">
+        <TranslatedText textKey='projects' className='hover:text-blue-500 ease-out transition-all duration-300 cursor-pointer' /></Link>
+        <TranslatedText textKey='contact' className='hover:text-blue-500 ease-out transition-all duration-300 cursor-pointer' />
         </ul>
       </div>
     </nav>
