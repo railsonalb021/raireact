7dca8ff06184c8954e04dcab160d3aeedbb9d1ea
Railson Albino <railsonvlogz@gmail.com>
2025-07-29 23:28:54 -0300

    🔧 refactor: replace static footer text with TranslatedText component for localization

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,10 @@
+import TranslatedText from '../components/TranslatedText'
+
 function Footer(){
   return (
-    <nav className="mt-72 bg-black p-6 text-white">
+    <nav className="mt-44 bg-black p-6 text-white">
       <div className="flex items-center justify-center">
-        <h1 className="cursor-pointer font-mono text-lg font-extralight transition-all duration-300 ease-in-out hover:text-blue-500">develop by mec.dev</h1>
+        <TranslatedText textKey='footer' className="-mt-9 cursor-pointer font-mono text-lg font-extralight transition-all duration-300 ease-in-out hover:text-blue-500"></TranslatedText>
       </div>
     </nav>
   )
