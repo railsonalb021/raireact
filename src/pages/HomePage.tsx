921aa0acf8f6f75ec100f737aacfd605a8286c5e
Railson Albino <railsonvlogz@gmail.com>
2025-07-29 23:29:13 -0300

    🔧 refactor: update text styles and layout for improved readability on HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,30 +5,29 @@ function HomePage(){
   return (
     <div>
       <div className="flex flex-col items-center justify-center">
-        <TranslatedText textKey="mec.dev" className="pt-20 font-mono text-xl text-blue-500" />
-        <TranslatedText textKey="frontenddevloper" className="font-mono text-white" />
+        <TranslatedText textKey="mec.dev" className="pt-20 font-mono text-2xl text-blue-500 lg:text-xl" />
+        <TranslatedText textKey="frontenddevloper" className="font-mono text-xl text-white" />
       </div>
       <div className="mt-10 flex flex-col items-center justify-center">
         <img src={minhaImagem} alt="Railson Albino" className="mb-4 h-32 w-48 rounded-full" />
       </div>
 
-      <div className='grid grid-cols-1 lg:grid-cols-3 lg:pt-20'>
-        <div className='flex flex-col items-start pl-10'>
-          <TranslatedText textKey="myskills" className='pb-3 pl-24 font-mono text-blue-500' />
+      <div className='grid grid-cols-1 pt-20 lg:grid-cols-3 lg:pt-20'>
+        <div className='flex flex-col items-center pb-10 pl-10 lg:items-start lg:pb-0'>
+          <TranslatedText textKey="myskills" className='mr-5 pb-3 font-mono text-xl text-blue-500 lg:mr-0 lg:pl-24 lg:text-base' />
           <TranslatedText textKey="myskillstext" className='font-mono text-white' />
         </div>
 
-        <div className='flex flex-col items-center pl-10'>
-          <TranslatedText textKey="aboutme" className='pb-3 pr-5 font-mono text-blue-500' />
+        <div className='flex flex-col items-center pb-10 pl-10 lg:pb-0'>
+          <TranslatedText textKey="aboutme" className='pb-3 pr-5 font-mono text-xl text-blue-500 lg:text-base' />
           <TranslatedText textKey='aboutmetext' className='font-mono text-white' />
         </div>
 
         <div className='flex flex-col items-center pl-10'>
-          <TranslatedText textKey='nextsteps' className='pb-3 pr-5 font-mono text-blue-500' />
+          <TranslatedText textKey='nextsteps' className='pb-3 pr-5 font-mono text-xl text-blue-500 lg:text-base' />
           <TranslatedText textKey="nextstepstext" className='font-mono text-white' />
         </div>
       </div>
-
     </div>
   )
 }
