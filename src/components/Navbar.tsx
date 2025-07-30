2b8e8ddbeb318b93bc06cb322a042eea1d9039ed
Railson Albino <railsonvlogz@gmail.com>
2025-07-29 23:28:58 -0300

    🔧 refactor: enhance Navbar layout and spacing for improved readability and localization

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,23 @@
-import TranslatedText from '../components/TranslatedText'
 import { Link } from 'react-router-dom'
+import TranslatedText from '../components/TranslatedText'
 
 function Navbar(){
   return (
-    <nav className="border-b-2 bg-black p-6 text-white">
+    <nav className="bg-black p-6 text-white">
       <div>
-        <TranslatedText textKey='mec.dev' className="cursor-pointer font-mono text-xl font-extralight transition-all duration-300 ease-in-out hover:text-blue-500"></TranslatedText>
+        <TranslatedText textKey='mec.dev' className="cursor-pointer pl-7 font-mono text-xl font-extralight transition-all duration-300 ease-in-out hover:text-blue-500 lg:text-base"></TranslatedText>
       </div>
       <div className="flex items-center justify-end">
-        <ul className="-mt-7 flex space-x-4 font-mono text-sm">
-        <Link to="/">
-        <TranslatedText textKey='home' className='hover:text-blue-500 ease-out transition-all duration-300 cursor-pointer' /></Link>
-        <TranslatedText textKey='about' className='hover:text-blue-500 ease-out transition-all duration-300 cursor-pointer' />
-        <Link to="/projetos">
-        <TranslatedText textKey='projects' className='hover:text-blue-500 ease-out transition-all duration-300 cursor-pointer' /></Link>
-        <TranslatedText textKey='contact' className='hover:text-blue-500 ease-out transition-all duration-300 cursor-pointer' />
+        <ul className="-mt-7 flex space-x-4 pr-9 font-mono text-xl lg:text-sm">
+          <Link to="/">
+            <TranslatedText textKey='home' className='cursor-pointer transition-all duration-300 ease-out hover:text-blue-500' /></Link>
+          <TranslatedText textKey='about' className='cursor-pointer transition-all duration-300 ease-out hover:text-blue-500' />
+          <Link to="/projetos">
+            <TranslatedText textKey='projects' className='cursor-pointer transition-all duration-300 ease-out hover:text-blue-500' /></Link>
+          <TranslatedText textKey='contact' className='cursor-pointer transition-all duration-300 ease-out hover:text-blue-500' />
         </ul>
       </div>
+      <div className='mt-5 overflow-hidden whitespace-nowrap border-t border-gray-800' />
     </nav>
   )
 }
